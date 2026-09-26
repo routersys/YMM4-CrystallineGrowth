b192ad8581d5a72820855d52861c34a62a3503bf
routersys <github.routersys@gmail.com>
2026-09-26 11:13:17 +0900

    更新確認で例外を握り潰さず報告する

--- CrystallineGrowth/CrystallineGrowthUpdateNotifier.cs
+++ CrystallineGrowth/CrystallineGrowthUpdateNotifier.cs
@@ -31,8 +31,10 @@ internal static class CrystallineGrowthUpdateNotifier
             };
             await new Notifier(Owner, Repository, options).NotifyAsync().ConfigureAwait(false);
         }
-        catch
+        catch (Exception exception)
         {
+            if (exception is not UpdateCheckException)
+                CrystallineGrowthTelemetry.Report(exception);
         }
     }
 

06f63c69342e82d75ad808449d9fbfbbc44af9c3
routersys <github.routersys@gmail.com>
2026-09-26 09:44:42 +0900

    シェーダーの生成をCrystallineGrowth.Shaderへ切り出す

--- CrystallineGrowth/ShaderResourceUri.cs
+++ CrystallineGrowth/ShaderResourceUri.cs
@@ -2,5 +2,5 @@ namespace CrystallineGrowth;
 
 internal static class ShaderResourceUri
 {
-    public static Uri Get(string shaderName) => new($"pack://application:,,,/CrystallineGrowth;component/Shaders/{shaderName}.cso", UriKind.Absolute);
+    public static Uri Get(string shaderName) => new($"pack://application:,,,/CrystallineGrowth;component/Resources/Shader/{shaderName}.cso", UriKind.Absolute);
 }
